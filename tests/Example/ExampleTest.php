f94553ff7d0d6d450e5ec64ad9a8dd2277b498ec
Owen Conti <owen@ohseemedia.com>
2022-03-26 11:06:27 -0600

    Updates to dependencies, fix LaravelServerAnalytics namespace occurences, add an example test

---
+++ tests/Example/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace OhSeeSoftware\LaravelPackageBoilerplate\Tests;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function example_test()
+    {
+        $this->assertTrue(true);
+    }
+}
